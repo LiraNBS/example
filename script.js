fbd913830840851e86e252e4cfc21fbc03770608
LiraN <liranbs98@gmail.com>
2021-10-21 15:21:25 +0300

    test

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("js file connected")
+
+const title = document.querySelector("h1")
+const words = document.querySelector(".words")
+const colors = document.querySelector(".colors")
+
+
+words
